effdffe51a54b3d40163776fa5338bac276dc858
Nima Mehrjoo <nmehrjoo1999@gmail.com>
2022-03-24 20:12:02 -0400

    list issues and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function(){
+    //grab repo name form url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName){
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    }else{
+        document.location.replace("./index.html")
+    }
+};
 var getRepoIssues = function(repo){
-    var apiUrl = "https://api.github.com/repos/"+ repo +"/issues?direction=asc"
+    var apiUrl = "https://api.github.com/repos/"+ repo +"/issues?direction=asc";
     fetch(apiUrl).then(function(response){
+        // request was successful
         if(response.ok){
             response.json().then(function(data){
                 //pass response data to dom function
@@ -15,7 +29,8 @@ var getRepoIssues = function(repo){
                 }
             });
         } else{
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -67,4 +82,4 @@ var displayWarning = function(repo){
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
